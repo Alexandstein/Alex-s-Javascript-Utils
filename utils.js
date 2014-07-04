41598c05202d7deddc62976f746937eec27c2bda
alexandstein <alexand.stein@gmail.com>
2014-07-03 20:30:43 -0700

    Added Namespacing and better documentation

--- utils.js
+++ utils.js
@@ -1,26 +1,67 @@
-//Replace all instances of a sequence in a string
-function replaceAll(find, replace, str) {
-	return str.replace(new RegExp(find, 'g'), replace);
-}
+var Utils = new function(){
+/************************************************
+Description:
+	Replace all instances of a string
+Args:
+	(string/regex) find
+		The search criterion
+	(string) replace
+		The replacement string
+	(string) str
+		The source string to search 
+Return:
+	(string) A new string with the proper replacements
+************************************************/
+	this.replaceAll = function(find, replace, str) {
+		return str.replace(new RegExp(find, 'g'), replace);
+	};
 
-//Find a key in an object given the value
-function getKey(obj, value){
-    keys = Object.keys(obj);
-    for(var i = 0; i < keys.length; i++){
-        if(obj[keys[i]] == value){
-             return keys[i];   
-        }
-    }
-    return undefined;
-}
+/************************************************
+Description:
+	Find a key in an object given the value
+Args:
+	(Object) obj
+		The search criterion
+	(any) value
+		The value to search for
+Return:
+	(string) The key of of the first value to match the search. Return 'undefined' 
+	if no such value is found.
+************************************************/
+	this.getKey = function(obj, value){
+		keys = Object.keys(obj);
+		for(var i = 0; i < keys.length; i++){
+			if(obj[keys[i]] == value){
+				 return keys[i];   
+			}
+		}
+		return undefined;
+	};
 
-//Capitalize first letter
-function capitalize(str)
-{
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
+/************************************************
+Description:
+	Capitalize first letter
+Args:
+	(string) str
+		The string to capitalize the first letter of
+Return:
+	(string) The key of of the first value to match the search. Return 'undefined' 
+	if no such value is found.
+************************************************/
+	this.capitalize = function(str){
+		return str.charAt(0).toUpperCase() + str.slice(1);
+	};
 
-//Strip whitespace
-function strip(str){
-	return string.replace(/(^\s+|\s+$)/g,'');
-}
+/************************************************
+Description:
+	Strip whitespace
+Args:
+	(string) str
+		String to strip whitespace of
+Return:
+	(string) String with whitespace stripped from it.
+************************************************/
+	this.strip = function(str){
+		return string.replace(/(^\s+|\s+$)/g,'');
+	};
+};
